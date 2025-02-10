d443b0ecccece509663f54a96e143ce5de29e856
riverDeer12 <mtrbojevic12@gmail.com>
2025-02-10 20:18:53 +0100

    working on container blocks and testimonal page

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
+            $('.sticky-top').addClass('bg-white shadow-sm').css('top', '0px');
         } else {
-            $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
+            $('.sticky-top').removeClass('bg-white shadow-sm').css('top', '-150px');
         }
     });
     
